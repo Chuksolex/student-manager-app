df44e838062764a5b4cbb1da61cfad4d1298d211
bazaacademy <infobazatutors@bazaacademy.com>
2026-05-02 23:32:10 +0100

    changes to config

--- db.js
+++ db.js
@@ -9,13 +9,14 @@ const mysql = require("mysql2"); //require means import
 //   password: process.env.DB_PASSWORD,
 //   database: process.env.DB_NAME,
 // });
-const connection = mysql.createConnection({
+const db= mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  database: process.env.DB_NAME,
 });
+
+//VERSION CONTROL: WHICH MEANS TRACKING ALL CHANGES
 
 db.connect((err) => {
      if (err) console.log("DB Error:", err); 
